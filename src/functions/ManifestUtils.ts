8f31b99cf79d760c41a574059480495dd4e400f6
AlexLEWIS <forerunner@outlook.com>
2025-07-20 00:49:07 +0800

    Add version-bump

--- src/functions/ManifestUtils.ts
+++ src/functions/ManifestUtils.ts
@@ -1,4 +1,5 @@
 import {readFileSync, writeFileSync} from "fs";
+import {pathExists} from "fs-extra";
 
 export function generateManifest(packagePath: string, manifestPath: string) {
     const packageJson = JSON.parse(readFileSync(packagePath, 'utf-8'));
@@ -17,4 +18,41 @@ export function generateManifest(packagePath: string, manifestPath: string) {
     );
 
     console.log(`✅  manifest.json generated with version ${manifest.version}`);
+}
+
+export async function versionBump(
+    packagePath: string,
+    targetManifestPath: string,
+    versionsPath: string) {
+
+    const packageJson = JSON.parse(readFileSync(packagePath, 'utf-8'));
+    const targetVersion = packageJson.obsidian?.version ?? packageJson.version;
+
+    let minAppVersion = packageJson.obsidian?.minAppVersion ?? '1.0.0';
+
+    // read minAppVersion from manifest.json if it exists
+    if (await pathExists(targetManifestPath)) {
+        let manifest = JSON.parse(readFileSync(targetManifestPath, 'utf-8'));
+
+        minAppVersion = manifest.minAppVersion;
+
+        manifest.version = targetVersion;
+
+        writeFileSync(
+            targetManifestPath,
+            JSON.stringify(manifest, null, 2),
+            'utf-8'
+        );
+
+        console.log(`✅  manifest.json updated with version ${targetVersion}`);
+    } else {
+        generateManifest(packagePath, targetManifestPath);
+    }
+
+    // update versions.json with target version and minAppVersion from manifest.json
+    let versions = JSON.parse(readFileSync(versionsPath, 'utf-8'));
+    versions[targetVersion] = minAppVersion;
+    writeFileSync(versionsPath, JSON.stringify(versions, null, '\t'), 'utf-8');
+
+    console.log(`✅  versions.json add with version ${targetVersion} and minAppVersion ${minAppVersion}`);
 }
